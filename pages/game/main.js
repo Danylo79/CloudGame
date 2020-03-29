2fb82a15b2e35b2734b0dd3a4c44b72ecd20452d
BuildTools <unconfigured@null.spigotmc.org>
2020-03-29 09:06:59 -0400

    Changed layout of site and Started creating the forum

--- pages/game/main.js
+++ pages/game/main.js
@@ -4,14 +4,26 @@ var getCookie = function (name) {
 	if (parts.length == 2) return parts.pop().split(";").shift();
 };
 
-fetch('/login/result').then(function (response) {
-	return response.json();
-}).then(function (data) {
-	console.log(data);
-	var btn = document.getElementById("loginbtn");
-	var isundefined = typeof(data.userid) == "undefined" || data.userid == null;
-	btn.innerHTML = isundefined ? "Login": "Logout";
-	btn.setAttribute("href", isundefined ? "/login": "/logout");
+fetch('/nav').then(function (response) {
+	response.text().then(function (text) {
+		var nav = document.getElementById("nav");
+		nav.innerHTML = text;
+	});
 }).catch(function (err) {
 	console.warn('Something went wrong.', err);
+}).then(function () {
+
+	fetch('/login/result').then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		console.log(data);
+		var btn = document.getElementById("loginbtn");
+		var isundefined = typeof (data.id) == "undefined" || data.id == null;
+		btn.innerHTML = isundefined ? "Login" : "Logout";
+		btn.setAttribute("href", isundefined ? "/login" : "/logout");
+		var label = document.getElementById("AccountName");
+		label.innerHTML = "Howdy " + data.name;
+	}).catch(function (err) {
+		console.warn('Something went wrong.', err);
+	});
 });
